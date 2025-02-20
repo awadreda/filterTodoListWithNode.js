d8c4b5637ac270735582a5a7cd37b52674e8dcda
Awad Reda <awadreda496@gmail.com>
2025-02-20 17:32:34 +0200

    optmize the code

--- filesPlaying.js
+++ filesPlaying.js
@@ -1,28 +1,24 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const allTasks = fs.readFileSync('awadTasks.md', 'utf-8');
-const tasks = allTasks.split('\n');
+const allTasks = fs.readFileSync("awadTasks.md", "utf-8");
+const tasks = allTasks.split("\n");
 
 function filterTasks() {
   const completedTasks = [];
   const uncompletedTasks = [];
 
   for (const task of tasks) {
-    if (task.includes('[ ]') || (! task.includes('[x]'))) {
-        if (!task.includes("[x]") && (! task.includes("[ ]")))
-        {
-            completedTasks.push(task);
+    if (!task.includes("[x]")) {
+      uncompletedTasks.push(task);
+    }
 
-        }
-            uncompletedTasks.push(task);
-
-    } else {
+    if (!task.includes(`[ ]`)) {
       completedTasks.push(task);
     }
   }
 
-  fs.writeFileSync('completedTasks.md', completedTasks.join('\n'), 'utf-8');
-  fs.writeFileSync('uncompletedTasks.md', uncompletedTasks.join('\n'), 'utf-8');
+  fs.writeFileSync("completedTasks.md", completedTasks.join("\n"), "utf-8");
+  fs.writeFileSync("uncompletedTasks.md", uncompletedTasks.join("\n"), "utf-8");
 }
 
 filterTasks();
